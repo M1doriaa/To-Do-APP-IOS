9339a26671c9f0d990f566268d5a48b5336c9d62
Lam <21010584@st.phenikaa-uni.edu.vn>
2025-07-05 21:48:46 +0700

    Initial Commit

---
+++ simpletodoapp/simpletodoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpletodoappApp.swift
+//  simpletodoapp
+//
+//  Created by Triệu Hải Lâm on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct simpletodoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
